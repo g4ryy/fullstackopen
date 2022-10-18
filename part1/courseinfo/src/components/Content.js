a3e239c646051e2f4dad845c8816f3540517c518
G <garyyansen@gmail.com>
2022-10-18 23:12:14 +0800

    Done part1a

---
+++ part1/courseinfo/src/components/Content.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Part from './Part'
+
+export default function Content(props) {
+    return (
+    <div>
+        <Part part={props.parts[0]} exercise={props.exercises[0]}/>
+        <Part part={props.parts[1]} exercise={props.exercises[1]}/>
+        <Part part={props.parts[2]} exercise={props.exercises[2]}/>
+    </div>
+    )
+}
